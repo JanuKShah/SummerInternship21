7683f945b47d28884552519f66dea54318a1baac
JanuKShah <71145203+JanuKShah@users.noreply.github.com>
2021-05-27 23:16:45 +0530

    Add files via upload

---
+++ Task2/Question2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "hello world";
+            s.Replace('o', 'i');
+            s.Replace("r", string.Empty);
+            Console.WriteLine(s);
+            Console.ReadLine();
+
+            /*
+             Output: hello world
+             Replace returns a new string after replacing 
+            all occurences of a given string with the
+            specified string.
+            But the original string is not modified.
+            To modify it, we have to reassign it the replaced string
+            i.e. s = s.Replace('o','i');
+             */
+        }
+    }
+}
